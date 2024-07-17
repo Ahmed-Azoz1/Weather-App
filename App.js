8e3c0b92026a2d572b2e20785967607b30259b89
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-07-17 23:43:19 +0300

    New Project

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-600">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+// import React from 'react';
+// import { View, Text } from 'react-native';
+
+
+// const App = () => {
+//   return (
+//       <View className="flex-1 justify-center items-center bg-blue-500">
+//         <Text className="text-white text-lg">
+//           Hello, NativeWind!
+//         </Text>
+//       </View>
+//   );
+// };
+
+// export default App;
+
