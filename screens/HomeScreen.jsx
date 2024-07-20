70f00bdd24293019de790bde621e2bb8c2da425d
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-07-20 20:25:46 +0300

    Search Loction

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,10 +1,18 @@
 import { View, Text, Image, SafeAreaView, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { theme } from '../theme';
 import {MagnifyingGlassIcon} from 'react-native-heroicons/outline'
+import {MapPinIcon} from 'react-native-heroicons/solid'
 
 const HomeScreen = () => {
+    const [showSearch,setShowSearch] = useState(false)
+    const [locations,setLocations] = useState([1,2,3])
+
+    const handleLocation = (item)=>{
+        
+    }
+
     return (
         <View className="flex-1 relative">
             <StatusBar style="light" />
@@ -12,12 +20,38 @@ const HomeScreen = () => {
             
             <SafeAreaView className="flex flex-1 pt-14 px-4">
                 <View style={{height:'7%'}}>
-                    <View style={{backgroundColor:theme.bgWhite(0.2)}} className="flex-row justify-end items-center rounded-full">
-                        <TextInput className="text-white text-base flex-1 h-10 pb-1 pl-6" placeholder='Search City' placeholderTextColor={'lightgray'}></TextInput>
-                        <TouchableOpacity className="rounded-full p-3 m-1" style={{backgroundColor:theme.bgWhite(0.3)}}>
+
+                    <View style={{backgroundColor: showSearch ? theme.bgWhite(0.2) : 'transparent'}} className="flex-row justify-end items-center rounded-full">
+                        {
+                            showSearch ? (
+                                <TextInput className="text-white text-base flex-1 h-10 pb-1 pl-6" placeholder='Search City' placeholderTextColor={'lightgray'} />
+                            ) : null
+                        }
+                        <TouchableOpacity onPress={()=>setShowSearch(!showSearch)} className="rounded-full p-3 m-1" style={{backgroundColor:theme.bgWhite(0.3)}}>
                             <MagnifyingGlassIcon size={25} color="white" />
                         </TouchableOpacity>
                     </View>
+                    {
+                        locations.length > 0 && showSearch ? 
+                        (
+                            <View className="absolute w-full bg-gray-200 top-16 rounded-3xl">
+                                {
+                                    locations.map((item,index)=>{
+                                        let showBorder = index+1 != locations.length;
+                                        let boderClass = showBorder ? 'border-b-2 border-b-gray-300' : '';
+                                        return  (
+                                            <TouchableOpacity onPress={()=>handleLocation(item)} key={index} className={"flex-row items-center border-0 p-3 px-4 mb-1"+boderClass}>
+                                                <MapPinIcon size={20} color="gray"/>
+                                                <Text className="text-black text-lg ml-2">London</Text>
+                                            </TouchableOpacity>
+                                        )
+                                    }
+                                    )
+                                }
+                            </View>
+                        ) : 
+                        null
+                    }
                 </View>
             </SafeAreaView>
             
